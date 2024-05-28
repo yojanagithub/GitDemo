82dfc9967652899095bb8da38c0b03500535d320
yojanagithub <yojanachalichalamala@gmail.com>
2024-05-28 07:25:20 +0530

    This is commit from Asian Timezone guy

--- src/test/java/Assignment.java
+++ src/test/java/Assignment.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class Assignment {
     public static void main(String[] args) {
@@ -21,5 +22,9 @@ public class Assignment {
 
 
 
+    }
+    @Test
+    public void TestforGit(){
+        System.out.println("testforgit");
     }
 }
